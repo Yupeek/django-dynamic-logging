0c8ce3d755f17c6cfc13f36c86ea220d528d8e4b
darius BERNARD <darius@yupeek.com>
2017-02-14 10:19:11 +0100

    added a default template page with some screenshots.

--- testproject/settings.py
+++ testproject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'testproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'testproject', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,6 +113,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "testproject", "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 
 LOGGING = {
